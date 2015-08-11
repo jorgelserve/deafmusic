2e9154887daaa7bfbd353b6a23ed9f949c0f0328
jorgelserve <jorgelserve@yahoo.es>
2015-08-11 16:35:08 -0500

    Code of basic son in C major

---
+++ toneMelody/toneMelody.ino
@@ -0,0 +1,137 @@
+#include "pitches.h"
+
+int C=2;
+int D=3;
+int E=4;
+int F=5;
+int G=6;
+int A=7;
+int B=8;
+int C2=9;
+int ASD=10;
+
+
+void setup () {
+  pinMode(C,OUTPUT);
+  pinMode(D,OUTPUT);
+  pinMode(E,OUTPUT);
+  pinMode(F,OUTPUT);
+  pinMode(G,OUTPUT);
+  pinMode(A,OUTPUT);
+  pinMode(B,OUTPUT);
+  pinMode(C2,OUTPUT);
+  pinMode(ASD,OUTPUT);
+}
+
+void loop () {
+  tone(ASD,NOTE_G5);
+  delay(500);
+  noTone(ASD);
+  tone(ASD,NOTE_C5);
+  delay(250);
+  noTone(ASD);
+  tone(ASD,NOTE_D5);
+  delay(250);
+  noTone(ASD);
+  tone(ASD,NOTE_E5);
+  delay(250);
+  noTone(ASD);
+  tone(ASD,NOTE_F5);
+  delay(250);
+  noTone(ASD);
+  tone(ASD,NOTE_G5);
+  delay(500);
+  noTone(ASD);
+  tone(ASD,NOTE_C5);
+  delay(500);
+  noTone(ASD);
+  delay(20);
+  tone(ASD,NOTE_C5);
+  delay(500);
+  noTone(ASD);
+  tone(ASD,NOTE_A5);
+  delay(500);
+  noTone(ASD);
+  tone(ASD,NOTE_F5);
+  delay(250);
+  noTone(ASD);
+  tone(ASD,NOTE_G5);
+  delay(250);
+  noTone(ASD);
+  tone(ASD,NOTE_A5);
+  delay(250);
+  noTone(ASD);
+  tone(ASD,NOTE_B5);
+  delay(250);
+  noTone(ASD);
+  tone(ASD,NOTE_C6);
+  delay(500);
+  noTone(ASD);
+  tone(ASD,NOTE_C5);
+  delay(500);
+  noTone(ASD);
+  tone(ASD,NOTE_C5);
+  delay(500);
+  noTone(ASD);
+  tone(ASD,NOTE_F5);
+  delay(500);
+  noTone(ASD);
+  tone(ASD,NOTE_G5);
+  delay(250);
+  noTone(ASD);
+  tone(ASD,NOTE_F5);
+  delay(250);
+  noTone(ASD);
+  tone(ASD,NOTE_E5);
+  delay(250);
+  noTone(ASD);
+  tone(ASD,NOTE_D5);
+  delay(250);
+  noTone(ASD);
+  tone(ASD,NOTE_E5);
+  delay(500);
+  noTone(ASD);
+  tone(ASD,NOTE_F5);
+  delay(250);
+  noTone(ASD);
+  tone(ASD,NOTE_E5);
+  delay(250);
+  noTone(ASD);
+  tone(ASD,NOTE_D5);
+  delay(250);
+  noTone(ASD);
+  tone(ASD,NOTE_C5);
+  delay(250);
+  noTone(ASD);
+  tone(ASD,NOTE_B4);
+  delay(500);
+  noTone(ASD);
+  tone(ASD,NOTE_C5);
+  delay(250);
+  noTone(ASD);
+  tone(ASD,NOTE_D5);
+  delay(250);
+  noTone(ASD);
+  tone(ASD,NOTE_E5);
+  delay(250);
+  noTone(ASD);
+  tone(ASD,NOTE_D5);
+  delay(250);
+  noTone(ASD);
+  tone(ASD,NOTE_E5);
+  delay(1500);
+  noTone(ASD);
+  tone(ASD,NOTE_C5);
+  delay(1500);
+  noTone(ASD);
+  
+  
+  
+  
+  
+  
+  delay(5000);
+  
+  
+  
+}
